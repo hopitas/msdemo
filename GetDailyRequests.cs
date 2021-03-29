f77af2075698bc2084f70f0761803c42c2fc0110
Tero Toivola <tero.toivola@leasegreen.fi>
2021-03-29 08:26:24 +0300

    fix to getdailyrequests

--- GetDailyRequests.cs
+++ GetDailyRequests.cs
@@ -3,20 +3,27 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace MS.Function
 {
     public class GetDailyRequests
     {
         [FunctionName("GetDailyRequests")]
-        public async void Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
         {
             string savelocation = req.Query["savelocation"];
             System.Net.WebClient client = new WebClient();
             DateTime now = DateTime.UtcNow;
-            string receivelocation = "containerurl" + "log_" + now.Date.Year.ToString() + now.Date.Month.ToString("00") + now.Date.Day.ToString("00") + ".csv";
-            client.DownloadFile(receivelocation, savelocation);
+            string receivelocation = "https://storageaccountmsdem9003.blob.core.windows.net/message-container/" + "log_" + now.Date.Year.ToString() + now.Date.Month.ToString("00") + now.Date.Day.ToString("00") + ".csv";
+            byte[] filebytes = client.DownloadData(receivelocation);
+            return new FileContentResult(filebytes, "application/octet-stream")
+            {
+                FileDownloadName = "Export.csv"
+            };
+
         }
     }
 }
